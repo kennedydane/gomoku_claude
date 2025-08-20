ad0a56efaefe93b4480ad2386278ea6a97706c21
Dane Kennedy <kennedy.dane@gmail.com>
2025-08-20 12:41:26 +0200

    [WIP]

---
+++ backend/web/templatetags/board_filters.py
@@ -0,0 +1,62 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def range_filter(value):
+    """Generate a range from 0 to value-1"""
+    try:
+        return range(int(value))
+    except (ValueError, TypeError):
+        return range(0)
+
+@register.filter  
+def sub(value, arg):
+    """Subtract arg from value"""
+    try:
+        return int(value) - int(arg)
+    except (ValueError, TypeError):
+        return 0
+
+@register.filter  
+def add(value, arg):
+    """Add arg to value"""
+    try:
+        return int(value) + int(arg)
+    except (ValueError, TypeError):
+        return 0
+
+@register.filter
+def get_letter(value, index):
+    """Get letter at index from string"""
+    try:
+        return value[int(index)]
+    except (IndexError, ValueError, TypeError):
+        return ""
+
+@register.filter
+def is_star_point(board_size, position):
+    """Check if position (row,col as 'row,col') is a star point"""
+    try:
+        board_size = int(board_size)
+        row, col = map(int, position.split(','))
+        
+        # Star points for different board sizes
+        if board_size == 9:
+            star_points = [(2,2), (2,6), (4,4), (6,2), (6,6)]
+        elif board_size == 13:
+            star_points = [(3,3), (3,9), (6,6), (9,3), (9,9)]
+        elif board_size == 15:
+            star_points = [(3,3), (3,11), (7,7), (11,3), (11,11)]
+        elif board_size == 19:
+            star_points = [(3,3), (3,9), (3,15), (9,3), (9,9), (9,15), (15,3), (15,9), (15,15)]
+        else:
+            # For other sizes, use center and quarter points
+            center = board_size // 2
+            quarter = board_size // 4
+            three_quarter = 3 * board_size // 4
+            star_points = [(quarter, quarter), (quarter, three_quarter), (center, center), (three_quarter, quarter), (three_quarter, three_quarter)]
+        
+        return (row, col) in star_points
+    except (ValueError, AttributeError):
+        return False
